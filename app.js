c0b821634fb26d156b5d9d09ee7d9e83133fe147
kdw9727@naver.com <kdw9727@naver.com>
2022-12-29 20:10:53 +0900

    toggle [style.css] [app.js]

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-// import "./styles.css";
-// const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-// <⚠️ /DONT DELETE THIS ⚠️>
+const h1 = document.querySelector("div.hello:first-child h1");
 
-/*
-✅ The text of the title should change when the mouse is on top of it.
-✅ The text of the title should change when the mouse is leaves it.
-✅ When the window is resized the title should change.
-✅ On right click the title should also change.
-✅ The colors of the title should come from a color from the colors array.
-✅ DO NOT CHANGE .css, or .html files.
-✅ ALL function handlers should be INSIDE of "superEventHandler"
-*/
-const title = document.querySelector("h2");
+function handleTitleClick() {
+  /*  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  } */
+  h1.classList.toggle("clicked");
+}
 
-  const superEventHandler = {
-    handleMouseEnter : function() {
-      title.innerText = "The mouse is here!";
-      title.style.color = "#1abc9c";
-    },
-    handleMouseLeave : function() {
-      title.innerText = "The mouse is gone!";
-      title.style.color = "#3498db";
-    },
-    handleRightClick : function() {
-      title.innerText = "That was a right click!";
-      title.style.color = "#9b59b6";
-    },
-    handleWindowResize : function() {
-      title.innerText = "You just resized!";
-      title.style.color = "#e74c3c";
-    },
-  };
-title.addEventListener("mouseenter", superEventHandler["handleMouseEnter"]);
-title.addEventListener("mouseleave", superEventHandler["handleMouseLeave"]); 
-title.addEventListener("contextmenu", superEventHandler["handleRightClick"]);
-window.addEventListener("resize", superEventHandler["handleWindowResize"]);
-
-
+h1.addEventListener("click", handleTitleClick);
